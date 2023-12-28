5a8ffa8bf9ae3ea47d5af71fd3190e88e5c8dda9
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2023-12-28 15:51:54 +0100

    Game provider without logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client"
 
 import Dashboard from "@/components/Dashboard/Dashboard";
-import Navigation from "@/components/Navigation/Navigation";
 import PrivateRoute from "@/components/PrivateRoute";
-import ProfileMenu from "@/components/ProfileMenu/ProfileMenu";
-import ThemeSelect from "@/components/ThemeSelect";
-import { useAuth } from "@/providers/Auth/AuthProvider"
 
 
 export default function Home() {
