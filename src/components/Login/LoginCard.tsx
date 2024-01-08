5fded5260b3b715d553f0f32f7506878a4b80eda
Michal Konvičný <michalkonvic@gmail.com>
2024-01-08 22:37:55 +0100

    Discord login

--- src/components/Login/LoginCard.tsx
+++ src/components/Login/LoginCard.tsx
@@ -8,10 +8,14 @@ import { Button } from '../ui/button';
 import { Form } from '../ui/form';
 import { formSchema } from './types';
 import { z } from 'zod';
+import { Separator } from '../ui/separator';
+import { DiscordLogoIcon } from '@radix-ui/react-icons';
+import { useAuth } from '@/providers/Auth/AuthProvider';
 
 
 
 const LoginCard = () => {
+    const { discordLogin } = useAuth();
     const methods = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -20,13 +24,19 @@ const LoginCard = () => {
         }
     });
   return (
-    <Card className="min-w-[350px]">
+    <Card className="min-w-[350px] md:w-[400px]">
         <Form {...methods}>
             <CardHeader>
                 <CardTitle className='w-full text-center'>
-                    Login
+                    Welcome back
                 </CardTitle>
             </CardHeader>
+            <CardContent className='w-full flex justify-center'>
+                <Button onClick={() => discordLogin()} variant={"outline"} className='px-12'>
+                    <DiscordLogoIcon className='w-5 h-5'/>
+                </Button>
+            </CardContent>
+            <Separator className='mb-5'/>
             <CardContent>
                 <LoginForm/>
             </CardContent>
